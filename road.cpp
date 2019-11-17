24b4f4537dd19c9ca07539e629dcd0e2919341b4
Tsiliii <32791023+Tsiliii@users.noreply.github.com>
2019-11-18 00:31:58 +0200

    Add files via upload

--- road.cpp
+++ road.cpp
@@ -4,15 +4,18 @@
 #include <unordered_map>
 
 using namespace std;
-int solve(int a,int b,int Ts,int Tf,int c,int d){
-  if(c-a*d<0){
+int solve(int Cs,int Cf,int Ts,int Tf,int c,int d){
+  if(c-d*Cs<0){
+    //cout << "Exit distance is " << d << endl;
     return -1;
   }
-  int temp = (c-a*d)/(b-a);
-  if(d - b*temp < 0){
+  int temp = (c-d*Cs)/(Cf-Cs);
+  while( (d-temp)< 0){
+      //cout << temp << endl;
       temp --;
   }
-  //cout << temp*Tf + (d-temp)*Ts << endl;
+  //cout << Cf << " " << temp << " " << Cs  << " " << d - temp << " " << Cf*temp << " " << Cs*(d-temp) << " " << d << " " << c << endl;
+  //cout << "Final time is " << temp*Tf + (d-temp)*Ts << " for the distance " << d <<  endl;
   return temp*Tf + (d-temp)*Ts;
 }
 
@@ -22,7 +25,7 @@ int main(int argc, char** argv){
   ifstream infile;
   infile.open(argv[1]);
   infile >>  N >> K >> D >> T ;
-  int * dist = new int[K];
+  int * dist = new int[K+1];
   int cost;
   int *cap = new int[N];
   for(int i=0; i<N; i++){
@@ -38,32 +41,50 @@ int main(int argc, char** argv){
   for(int i=0; i<K; i++){
     infile >> dist[i];
   }
+  dist[K+1] = D;
+  sort(dist,dist+K+1);
+  int tempe=dist[0];
+  for(int l=1; l<K+1; l++){
+    tempe = max(tempe,dist[l]-dist[l-1]);
+  }
+  if(cap[N-1]<tempe){
+    //cout << cap[N-1] << " " << tempe << endl;
+    cout << -1 << endl;
+    return 0;
+  }
+  //cout << tempe << endl;
+  //return 0;
   infile >> Ts >> Cs >> Tf >> Cf;
   int temp;
   int distance;
-  int step = N;
+  int step = N/2;
   currentN = N/2;
   while(step!=0){
     step = step / 2;
     currentD = 0;
     time = 0;
     distance = 0;
-        for(j=0; j<K+1; j++){
-        if(j!=K){
-            distance = dist[j] + distance;
-            temp = solve(Cs,Cf,Ts,Tf,cap[currentN],dist[j]);
-        }
-        else{
-            distance = D - distance;
-            temp = solve(Cs,Cf,Ts,Tf,cap[currentN],distance);
-        }
-        if(temp==-1){
+        for(j=0; j<K; j++){
+          if(cap[currentN]<tempe){
+            temp=-1;
             break;
+          }
+          distance = dist[j+1] - dist[j];
+          int what = j;
+          while(distance/cap[currentN] < Cf && j<K){
+            distance = dist[j++] - dist[what];
+          }
+          cout << endl << "Distance is " << distance << " ,capacity is " << cap[currentN] << endl;
+                  cout << "CurrentN = " << currentN << endl;
+          temp = solve(Cs,Cf,Ts,Tf,cap[currentN],distance);
+          if(temp==-1 || time > T){
+              break;
+            }
+            time = time + temp;
+            cout << "Time = " <<  time  << " ,CurrentN = " << currentN << " ,Capacity = "<< cap[currentN] << " ,Current station is " << dist[j] << endl;
+            currentD = dist[j];
         }
-        time = time + temp;
-        //cout << "Time = " <<  time  << " currentN = " << currentN << endl;
-        currentD = dist[j];
-        }
+        //cout << "STOP" << endl;
         if(temp == -1 || time > T){
             if(step!=0){
                 currentN = currentN+step;
@@ -74,9 +95,12 @@ int main(int argc, char** argv){
             }
             continue;
         }
+        cout << "CurrentN = " << currentN << endl;
         currentN = currentN-step;
+      //  cout << endl;
     }
-    //cout << "Last call bitches" << endl;
+/*    return 0;
+    cout << "Last call bitches" << endl;
     currentD = 0;
     time = 0;
     distance = 0;
@@ -97,12 +121,18 @@ int main(int argc, char** argv){
     //cout << "Time = " <<  time  << " currentN = " << currentN << endl;
     currentD = dist[j];
     }
+    */
     if (currentN >= N){
         cout << -1 <<endl;
         return 0;
     }
     else{
-        cout << Costs[cap[currentN]] << endl;
+      int temp1 = Costs[cap[currentN]];
+        for(int i=currentN; i<N; i++){
+          temp1 = min(Costs[cap[i]],temp1);
     }
+    cout << temp1 << endl;
+    return 0;
+  }
   return 0;
 }
