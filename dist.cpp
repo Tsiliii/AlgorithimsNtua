2e90ea04b4399a33997efe9c5d23e350a419047a
Tsiliii <32791023+Tsiliii@users.noreply.github.com>
2020-01-24 15:15:18 +0200

    Add files via upload

--- dist.cpp
+++ dist.cpp
@@ -61,26 +61,66 @@ void mymakeset(int x, int *parent, int *rank){
   rank[x] = 0;
 }
 
-int main(/*int argc, char** argv*/){
-//  for(int x=1; x<=24; x++){
-//    string temp1= "in" +create(x);
-//    string temp2= "out" + create(x);
-    int answer = 0;
-    int N,K;
-    //ifstream infile;
-    //infile.open(temp1);
-    //infile >> N >> K ;
-    cin >>  N >> K ;
-    int tmp1,tmp2,tmp3;
-    list<bi> *arr = new list<bi>[N+1];
-    for(int i=0; i<K; i++){
-      cin >> tmp1 >> tmp2;
-      arr[tmp1].push_front(bi(tmp2,tmp3));
-      arr[tmp2].push_front(bi(tmp1,tmp3));
-    }
-    int *array = new int[N+1];
+int main(int argc, char** argv){
+  for(int x=0; x<=0; x++){
+    string temp1= "in" +create(x);
+    string temp2= "out" + create(x);
+    int N,M;
+    ifstream infile;
+    infile.open(temp1);
+    infile >> N >> M ;
+    //cin >>  N >> M ;
+    list<int> *tree = new list<int>[N+1];
+    int *sortededge1 = new int[M];
+    int *sortededge2 = new int[M];
     int *parent = new int[N+1];
     int *rank = new int[N+1];
-
+    int tmp1,tmp2,tmp3;
+    int *node_count = new int[N+1];
+    bool *stays = new bool[M];
+    queue <int> leaves;
+    for(int i=1; i<N+1; i++){
+      mymakeset(i,parent,rank);
+      node_count[i] = 0;
+    }
+    for(int i=0; i<M; i++){
+      //cin >> tmp1 >> tmp2 >> tmp3;
+      infile >> tmp1 >> tmp2 >> tmp3;
+      sortededge1[tmp3] = tmp1;
+      sortededge2[tmp3] = tmp2;
+      stays[i] = false;
+    }
+    int ConnComp = N;
+    int i = 0;
+    while(ConnComp>1){
+      if(myfind(sortededge1[i],parent,rank)!=myfind(sortededge2[i],parent,rank)){
+        tree[sortededge1[i]].push_front(sortededge2[i]);
+        node_count[sortededge1[i]]++;
+        node_count[sortededge2[i]]++;
+        tree[sortededge2[i]].push_front(sortededge1[i]);
+        myunion(sortededge1[i],sortededge2[i],parent,rank);
+        ConnComp--;
+        stays[i] = false;
+      }
+      i++;
+    }
+    for(int i=1; i<N+1; i++){
+      if (node_count[i] == 1){
+          leaves.push(i);
+      }
+    }
+    for(int i=1; i<N+1; i++){
+        for (list<int>::iterator it=tree[i].begin(); it != tree[i].end(); ++it){
+          cout << i << " " << *it << endl;
+        }
+        cout << "Node " << i << " has " <<  node_count[i] << endl;
+    }
+    // mymakeset(1,parent,rank);
+    // mymakeset(2,parent,rank);
+    // mymakeset(3,parent,rank);
+    // myunion(1,2,parent,rank);
+    // myunion(3,2,parent,rank);
+    // cout << myfind(1,parent,rank) << " " << rank[1] << endl << myfind(2,parent,rank) << " " << rank[2] << endl << myfind(3,parent,rank) << " " << rank[3] << endl;
+  }
   return 0;
   }
